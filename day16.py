f6e93a16c61fd09e3fba74c67e14ab9dcbed470a
Chuong.Nguyen <chuong.nguyen@nakivo.com>
2021-09-09 18:00:36 +0700

    Day17: get_data function

--- day16.py
+++ day16.py
@@ -2,6 +2,23 @@ import csv
 import shutil
 from tempfile import NamedTemporaryFile
 
+def read_data(user_id=None, user_email=None):
+    file_name = 'data.csv'
+    with open(file_name, 'r') as csvFile:
+        reader = csv.DictReader(csvFile)
+        for row in reader:
+            if user_id is not None:
+                if int(user_id) == int(row.get('id')):
+                    return row
+                else:
+                    return "User id {user_id} not found".format(user_id=user_id)
+            elif user_email is not None:
+                if user_email == row.get('email'):
+                    return row
+                else:
+                    return "User email {user_email} not found".format(user_email=user_email)
+    return None
+
 def append_data(file_path, name, email):
     field_names = ['id', 'name', 'email']
     next_id = get_length(file_path)
@@ -20,7 +37,7 @@ def get_length(file_path):
         data_list = list(reader)
         return len(data_list)
 
-def edit_data(id, amount, sent):
+def edit_data(edit_id, email=None, amount=None, sent=None):
     file_name = 'data.csv'
     temp_file = NamedTemporaryFile(mode='w', delete=False)
     with open(file_name, 'r') as csvFile, temp_file:
@@ -29,10 +46,15 @@ def edit_data(id, amount, sent):
         writer = csv.DictWriter(temp_file, fieldnames=field_names)
         writer.writeheader()
         for row in reader:
-            if (int(row['id']) == id):
-                row['sent'] = sent
-                row['amount'] = amount
+            if edit_data is not None:
+                if (int(row['id']) == edit_id):
+                    row['sent'] = sent
+                    row['amount'] = amount
+            elif email is not None:
+                if (str(row['email']) == email):
+                    row['sent'] = sent
+                    row['amount'] = amount
             writer.writerow(row)
         shutil.move(temp_file.name, file_name)
 
-edit_data(2, 8884.3, True)
+print(read_data())
